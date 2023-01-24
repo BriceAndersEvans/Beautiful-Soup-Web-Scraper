b5b39fb677c9d347b6cce2e911cd026f3e51e792
BriceAndersEvans <102760105+BriceAndersEvans@users.noreply.github.com>
2023-01-24 14:10:40 -0500

    Created loop and if conditional for mutiple job listings

--- Web Scraper/main.py
+++ Web Scraper/main.py
@@ -5,16 +5,20 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
 #print(html_text)
 
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 # print(job)
-company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
-skills = job.find('span', class_='srp-skills').text.replace(' ','')
-posted_date = job.find('span', class_='sim-posted').span.text
-# print(company_name)
-# print(skills)
-print(posted_date)
+for job in jobs:
+    posted_date = job.find('span', class_='sim-posted').span.text
+    if 'few' in posted_date:
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+        skills = job.find('span', class_='srp-skills').text.replace(' ','')
+        # print(company_name)
+        # print(skills)
+        # print(posted_date)
 
-# print(f'''
-# Company Name: {company_name}
-# Required Skills: {skills}
-# ''')
+        print(f'''
+        Company Name: {company_name}
+        Required Skills: {skills}
+        ''')
+
+        print("")
