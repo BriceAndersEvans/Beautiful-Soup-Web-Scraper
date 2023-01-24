d4072d42a039d983b2e4f93a7386d94a9c1ad5e9
BriceAndersEvans <102760105+BriceAndersEvans@users.noreply.github.com>
2023-01-24 17:42:47 -0500

    Added a filter to job skills to filter out select skills

--- Web Scraper/main.py
+++ Web Scraper/main.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
+print('Specufy skills you would like to filter out of job listings: ')
+undeq_skill = input('>')
+print(f'Filtering out: {undeq_skill}')
+
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 #print(html_text)
-
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 # print(job)
@@ -16,8 +19,9 @@ for job in jobs:
         # print(company_name)
         # print(skills)
         # print(posted_date)
+        if undeq_skill not in skills:
+            print(f'Company Name: {company_name.strip()}')
+            print(f'Required Skills: {skills.strip()}')
+            print(f'Job Posting Link: {posting_url}')
 
-        print(f'Company Name: {company_name.strip()}')
-        print(f'Required Skills: {skills.strip()}')
-        print(f'Job Posting Link: {posting_url}')
-        print('')
+            print('')
